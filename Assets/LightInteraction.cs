ee7f48cf8f799dafdc141ca16da78535456557f8
Ceyb <s.di-giovanni@hotmail.fr>
2017-01-31 02:11:50 +0100

    Unity project added.

---
+++ Assets/LightInteraction.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LightInteraction : MonoBehaviour {
+
+	private GameObject light;
+	private BoxCollider lightCollider;
+	private bool on;
+	private bool showOnLabel;
+	private bool showOffLabel;
+
+	// Use this for initialization
+	void Start () {
+		light = GameObject.Find("Light0");
+		lightCollider = light.GetComponent<BoxCollider>();
+		on = true;
+		showOnLabel = false;
+		showOffLabel = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerStay(Collider other) {
+		if (other == lightCollider)
+		{
+			if (on == true)
+			{
+				if (showOffLabel == false)
+				{
+					showOffLabel = true;
+				}
+				if (Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.JoystickButton0))
+				{
+					light.GetComponent<Light>().intensity = 0;
+					on = false;
+					showOffLabel = false;
+				}
+			}
+			else
+			{
+				if (showOnLabel == false)
+				{
+					showOnLabel = true;
+				}
+				if (Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.JoystickButton0))
+				{
+					light.GetComponent<Light>().intensity = 1;
+					on = true;
+					showOnLabel = false;
+				}
+			}	
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if (other == lightCollider)
+		{
+			if (showOnLabel == true)
+			{
+				showOnLabel = false;
+			}
+			if (showOffLabel == true)
+			{
+				showOffLabel = false;
+			}
+		}
+    }
+	
+	void OnGUI() {
+		if (showOnLabel == true)
+		{
+			GUI.Label(new Rect(20, 10, 250, 40), "Press L (keyboard) or A (windows controller) to switch on the light.");
+		}
+		else if (showOffLabel == true)
+		{
+			GUI.Label(new Rect(20, 10, 250, 40), "Press L (keyboard) or A (windows controller) to switch off the light.");
+		}
+    }
+}
---
+++ Assets/PianoInteraction.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PianoInteraction : MonoBehaviour {
+
+	private GameObject piano;
+	private BoxCollider pianoCollider;
+	private bool playingPiano;
+	private bool showStartPianoLabel;
+	private bool showStopPianoLabel;
+
+	// Use this for initialization
+	void Start () {
+		piano = GameObject.Find("piano");
+		pianoCollider = piano.GetComponent<BoxCollider>();
+		playingPiano = false;
+		showStartPianoLabel = false;
+		showStopPianoLabel = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playingPiano == true)
+		{
+			if (showStopPianoLabel == false)
+			{
+				showStopPianoLabel = true;
+			}
+			if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.JoystickButton0))
+			{
+				piano.GetComponent<AudioSource>().Stop();
+				playingPiano = false;
+				showStopPianoLabel = false;
+			}
+		}
+		else if (showStartPianoLabel == true && (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.JoystickButton0)))
+		{
+			piano.GetComponent<AudioSource>().Play();
+			playingPiano = true;
+			showStartPianoLabel = false;
+		}
+	}
+	
+	void OnTriggerStay(Collider other) {
+		if (other == pianoCollider && playingPiano == false)
+		{
+			if (showStartPianoLabel == false)
+			{
+				showStartPianoLabel = true;
+			}
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if (other == pianoCollider && showStartPianoLabel == true)
+		{
+			showStartPianoLabel = false;
+		}
+    }
+	
+	void OnGUI() {
+		if (showStartPianoLabel == true)
+		{
+			GUI.Label(new Rect(20, 10, 200, 40), "Press P (keyboard) or A (windows controller) to play piano !");
+		}
+		else if (showStopPianoLabel == true)
+		{
+			GUI.Label(new Rect(20, 10, 250, 40), "Press P (keyboard) or A (windows controller) to stop playing piano.");
+		}
+    }
+}
